e9d3e97581c7e6b1f1a2c374c238ca89c4f1bfa6
Palak Mehrotra <palakmehrotra0612@gmail.com>
2025-03-28 11:43:53 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("#contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thanks for reaching out! We’ll get back to you soon.");
+});
